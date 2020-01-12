bb77b91f22a6814340c8da52ab3fb56144971bab
Dustin Mikusko <dustinjm713@yahoo.com>
2020-01-12 12:39:06 -0700

    Style landing page

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -20,13 +20,16 @@ export class Landing extends Component  {
       <main>
         <section className='landing-left'>
           <img src={logo} className='logo' alt='mlb-logo'/>
-          <h1>Baseball for <span className='dummies'>DUMMIES</span></h1>
+          <h1>Baseball<br /> for<br /></h1>
+          <h1 className='dummies'>DUMMIES</h1>
         </section>
         <section className='landing-right'>
-          <h2>New to baseball?</h2>
-          <p>Step up to the plate today to get a crash course on baseball stats! With our cutting edge technology, we bring you a simple black and white formula to tell you which players will be a HOME RUN in your draft, and those that are sure outs..</p>
-          <Link to='/signin'>Sign In</Link>
-          <Link to='/signup'>Sign Up</Link>
+          <h2 className='headline'>New to baseball?</h2>
+          <p className='intro'>Step up to the plate today to get a crash course on baseball stats! With our cutting edge technology, we bring you a simple black and white formula to tell you which players will be a HOME RUN in your draft, and those that are sure outs..</p>
+          <div className='buttons'>
+          <Link to='/signin' className='sign-in'>Sign In</Link>
+          <Link to='/signup' className='sign-up'>Sign Up</Link>
+          </div>
         </section>
       </main>
     )
